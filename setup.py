c75445b369ee9fd5ff0328b2a50a7571b00ff0a7
tycho garen <garen@tychoish.com>
2013-03-30 13:21:35 -0400

    release prep

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
+import buildcloth
 
 setup(
     name='buildcloth',
     description='A framework for genarting build system description files, with support for Ninja and Make.',
-    version='0.1-dev',
+    version=buildcloth.__version__,
     author='Sam Kleinman',
     author_email='sam@tychoish.com',
     license='Apache',
     url='http://cyborginstitute.org/projects/buildcloth',
-    packages=['buildergen'],
+    packages=['buildcloth'],
     setup_requires=['nose'],
-    test_suite='test'
+    test_suite='test',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache',
+        'License :: OSI Approved :: Apache Software License',
         'Topic :: Software Development :: Build Tools'
     ],
     )
